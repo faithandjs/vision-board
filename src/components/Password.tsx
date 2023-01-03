1101663f5fa12a5cc7f29dab5990a0099528a9a1
faithandjs <faiththe1rst@gmail.com>
2023-01-03 01:27:00 +0100

    password

---
+++ src/components/Password.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+
+export default function Password({
+  setMe,
+  me,
+  modal,
+  setModal,
+}: {
+  setMe: React.Dispatch<React.SetStateAction<boolean>>;
+  me: boolean;
+  setModal: React.Dispatch<React.SetStateAction<boolean>>;
+  modal: boolean;
+}) {
+  const [yes, setyes] = useState(false);
+  const password = process.env.REACT_APP_PASSWORD;
+  useEffect(() => {
+    localStorage.setItem('my-vision-board?', JSON.stringify(me));
+  }, [me]);
+
+  return (
+    <>
+      {' '}
+      {modal ? (
+        <div className='absolute h-screen inset-0 w-full flex justify-center items-center password text-gray-700'>
+          <div className='rounded-lg p-6 bg-white  w-80 '>
+            <h5>Do you know the password</h5>
+            <form
+              className='pt-4'
+              onSubmit={(e) => {
+                e.preventDefault();
+                if (yes) setMe(true);
+                else setMe(false);
+
+                setModal(false);
+              }}>
+              <input
+                type='password'
+                name='passowrd'
+                id='password'
+                className='border-b-2 w-full py-2 outline-0'
+                placeholder='Enter password'
+                onChange={(e) => {
+                  const { value } = e.target;
+                  console.log(value, password, yes, value === password);
+                  if (value === password) setyes(true);
+                  else setyes(false);
+                }}
+              />
+              <div className='child:px-4 child:py-2 w-full flex justify-end mt-5 child:border child:rounded-md'>
+                <button
+                  className='text-gray-700 mr-2'
+                  type='button'
+                  onClick={() => {
+                    setModal(false);
+                    setMe(false);
+                  }}>
+                  No
+                </button>
+                <button className='text-green-600' type='submit'>
+                  Yes
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+}
+
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,14 +8,24 @@ import '../../styles/global.scss';
 import '../../styles/theme.scss';
 
 import { useToggleCtx } from '../../context/ToggleCtx';
+import Password from '../../components/Password';
 
 export default function Home() {
-  const password = process.env.REACT_APP_PASSWORD;
-  // const data = JSON.parse(process.env.REACT_APP_DATA!);
+  // console.log(localStorage.getItem('my-vision-board?') !== undefined);
+  const details = JSON.parse(process.env.REACT_APP_DATA!);
+  const me_value =
+    localStorage.getItem('my-vision-board?') !== 'undefined'
+      ? JSON.parse(localStorage.getItem('my-vision-board?')!)
+      : false;
+  const LS_me = localStorage.getItem('my-vision-board?') ? false : true;
   const { states } = useToggleCtx();
   const { vision_board } = states;
   const [active, setActive] = useState(-1);
-  const { data, theme } = details2;
+  const [me, setMe] = useState<boolean>(me_value);
+  const [modal, setModal] = useState(LS_me);
+
+  const holder = me ? details : details2;
+  const { data, theme } = holder;
 
   useEffect(() => {
     if (vision_board) {
@@ -24,60 +34,79 @@ export default function Home() {
       document.body.classList.remove('vision-body');
     }
   }, [states]);
-
-  // console.log(details, password);
+  useEffect(() => {
+    console.log(me, localStorage.getItem('my-vision-board?'));
+  }, [me]);
+  console.log(me_value, LS_me);
   return (
-    <div
-      className={
-        'md:px-6 py-10  ' +
-        (states.vision_board
-          ? 'vision-board '
-          : 'not-vision-board border-8 border-[#2f2f2f] border-double ')
-      }>
-      <div
-        className={
-          'z-20 relative  transition-all duration-700 py-4  flex w-full flex-col text-center ' +
-          (states.vision_board
-            ? 'text-white   absolute inset-0 translate-y-[40vh]'
-            : '  delay-150 ')
-        }>
-        <h1 className={'font-deraga font-thin  text-5xl '}>My 2023</h1>
-        <p className={'font-alexander font-medium text-xl pb-4  '}>{theme}</p>
-      </div>
-      {/* cards */}
-      <div className='cards flex justify-center child:shrink-0 flex-wrap  transition-all duration-700'>
-        {data.map((item: any, key: number) => {
-          return (
-            <Vision {...{ ...item, active, setActive }} key={key} id={key} />
-          );
-        })}
-      </div>
+    <>
+      {!modal ? (
+        <div
+          className={
+            'md:px-6 py-10  ' +
+            (states.vision_board
+              ? 'vision-board '
+              : 'not-vision-board border-8 border-[#2f2f2f] border-double ')
+          }>
+          <div
+            className={
+              'z-20 relative  transition-all duration-700 py-4  flex w-full flex-col text-center ' +
+              (states.vision_board
+                ? 'text-white   absolute inset-0 translate-y-[40vh]'
+                : '  delay-150 ')
+            }>
+            <h1 className={'font-deraga font-thin  text-5xl '}>My 2023</h1>
+            <p className={'font-alexander font-medium text-xl pb-4  '}>
+              {theme}
+            </p>
+          </div>
+          {/* cards */}
+          {data && (
+            <div className='cards flex justify-center child:shrink-0 flex-wrap  transition-all duration-700'>
+              {data.map((item: any, key: number) => {
+                return (
+                  <Vision
+                    {...{ ...item, active, setActive }}
+                    key={key}
+                    id={key}
+                  />
+                );
+              })}
+            </div>
+          )}
 
-      {/* grid */}
-      <div className='grid transition-all duration-700 absolute inset-0 w-full h-screen  brightness-75'>
-        {data.map((item: any, key: number) => {
-          return (
-            <div
-              className={`grid-item${key + 1}`}
-              style={{ backgroundImage: `url(${item.image.src})` }}></div>
-          );
-        })}
-      </div>
-      <div className='fixed bottom-0 left-0 w-full flex justify-between px-5 pb-4 z-20'>
-        <Toggle
-          label='vision board'
-          position='bottom'
-          checked={states.vision_board}
-        />
-        <Toggle
-          label='flip all'
-          position='top'
-          checked={states.flip_all}
-          disabled={states.vision_board}
-          className={states.vision_board ? 'opacity-0' : 'opacity-100'}
-        />
-      </div>
-    </div>
+          {/* grid */}
+          {data && (
+            <div className='grid transition-all duration-700 absolute inset-0 w-full h-screen  brightness-75'>
+              {data.map((item: any, key: number) => {
+                return (
+                  <div
+                    className={`grid-item${key + 1}`}
+                    style={{ backgroundImage: `url(${item.image.src})` }}></div>
+                );
+              })}
+            </div>
+          )}
+          <div className='fixed bottom-0 left-0 w-full flex justify-between px-5 pb-4 z-20'>
+            <Toggle
+              label='vision board'
+              position='bottom'
+              checked={states.vision_board}
+            />
+            <Toggle
+              label='flip all'
+              position='top'
+              checked={states.flip_all}
+              disabled={states.vision_board}
+              className={states.vision_board ? 'opacity-0' : 'opacity-100'}
+            />
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+      <Password {...{ setMe, me, modal, setModal }} />
+    </>
   );
 }
 
